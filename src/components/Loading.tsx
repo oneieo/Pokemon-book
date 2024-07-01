5f922b449a826f36737f971028105758ec3168da
oneieo <sun020716@gmail.com>
2024-07-02 00:05:34 +0900

    update: rendering pokemon's name

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div>
+      <h1>포켓몬 깨우는 중 ...</h1>
+      <img src="" />
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { GET } from "@/app/api/pokemons/route";
+import { Pokemon } from "@/app/types/pokemon.type";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
@@ -12,13 +13,32 @@ const PokemonList = () => {
   } = useQuery({
     queryKey: ["pokemonData"],
     queryFn: async () => {
-      const response = await axios.get("http://localhost:3000/api/pokemons");
-      return response; // 헐 대박 !!!!!!!!!1 감사해요 혜미님 ㅜㅜㅜㅜㅜㅜㅜ
+      const { data } = await axios.get<Pokemon[]>(
+        "http://localhost:3000/api/pokemons"
+      );
+      return data; // 헐 대박 !!!!!!!!!1 감사해요 혜미님 ㅜㅜㅜㅜㅜㅜㅜ
     },
   });
 
+  if (isPending) {
+    return <>로딩중~</>;
+  }
+  if (isError) {
+    return <>데이터 조회 중 에러 발생~</>;
+  }
+
   console.log(pokemonData);
-  return <div>포켓몬리스트 컴포넌트입니다</div>;
+  return (
+    <ul>
+      {pokemonData.map((data) => {
+        return (
+          <li key={data.id}>
+            <h1>{data.korean_name}</h1>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default PokemonList;
